c7514f582a61f9e420c37fb2f1128d5ae1dbc51b
tpangestu <tripangestu28@gmail.com>
2025-10-17 19:32:32 +0700

    move assets to public folder and update paths

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -20,7 +20,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
 <a class="navbar-brand" href="{{ url('/') }}">
-    <img src="{{ asset('storage/bg/logobunny.png') }}" alt="Logo Bunny Care" class="me-2">
+    <img src="{{ asset('assets/bg/logobunny.png') }}" alt="Logo Bunny Care" class="me-2">
     Bunny Care
 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">

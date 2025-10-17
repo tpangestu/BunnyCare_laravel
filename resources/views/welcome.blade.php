93f58fd25bcfed217620b76b21236d219b8db18f
tpangestu <tripangestu28@gmail.com>
2025-10-17 18:26:51 +0700

    Fix asset loading in production: update @vite directives to use 'build' manifest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'], 'build')
 
     </head>
     <body class="antialiased">
